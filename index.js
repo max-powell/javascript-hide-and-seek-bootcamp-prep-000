a6521d9bd71de609d0029fe419618279a4df047a
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-21 16:33:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target')
 }
+
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < ranks.length; i++) {
+  ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+}
