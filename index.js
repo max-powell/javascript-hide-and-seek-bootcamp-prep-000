b785dd22629968084ec78a443ef81b1015886879
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-21 17:20:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ function deepestChild() {
   var current = divGN;
   while (current) {
     if (current.querySelector(':only-child')) {
-      //current = current.querySelector(':only-child');
-      return 'fun'
+      current = current.querySelector(':only-child');
     } else {
       return current
     }
